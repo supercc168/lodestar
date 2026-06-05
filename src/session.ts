b78707c8d328c535454867e1d237918f3e98e23d
leviyuan <leviyuan@qq.com>
2026-06-06 01:58:22 +0800

    refactor: tighten hi panel summaries

--- src/session.ts
+++ src/session.ts
@@ -1291,7 +1291,12 @@ export class Session {
       status: this.status,
       peers: [...Session.all]
         .filter(s => s.isRunning())
-        .map(s => ({ ...s.peerSnapshot(), isCurrent: s === this })),
+        .map(s => ({
+          ...s.peerSnapshot(),
+          isCurrent: s === this,
+          model: s === this ? (s.currentModelLabel() ?? undefined) : undefined,
+          effort: s === this ? s.currentEffortLabel() : undefined,
+        })),
       usage,
       sysinfo,
     }
